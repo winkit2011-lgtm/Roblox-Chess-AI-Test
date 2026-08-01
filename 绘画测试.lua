c547e09d0b35769c155db6ee34a265c9986f0129
winkit2011-lgtm <winkit2011@gmail.com>
2026-08-02 07:08:12 +0800

    Update 绘画测试.lua

--- 绘画测试.lua
+++ 绘画测试.lua
@@ -1,1 +1,9 @@
-print(game:GetService("Players").LocalPlayer)
+print("测试开始")
+
+local ok, result = pcall(function()
+	return game:GetService("Players")
+end)
+
+print("Players服务:", ok, result)
+
+print("测试结束")
